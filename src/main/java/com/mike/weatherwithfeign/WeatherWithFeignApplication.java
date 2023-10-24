88a001b3c69746864607eaf46d76732076cf4121
Mike <mick_mick_mick@icloud.com>
2023-10-24 20:15:29 +0400

    Initial commit

---
+++ src/main/java/com/mike/weatherwithfeign/WeatherWithFeignApplication.java
@@ -0,0 +1,13 @@
+package com.mike.weatherwithfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherwithfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherwithfeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/weatherwithfeign/WeatherWithFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.weatherwithfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherwithfeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
